e52e437f923e5c030cdb2354ede2fd89668d4b96
fengJA <m15881760105@163.com>
2020-02-20 17:43:50 +0800

    PageHelper搭建环境

---
+++ pagehelper/src/test/java/com/fj/pagehelper/PagehelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fj.pagehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagehelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
