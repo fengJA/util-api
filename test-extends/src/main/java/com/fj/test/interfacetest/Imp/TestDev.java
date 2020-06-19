f02a17855ede57cefd372ae15b9ac3d406e834a4
ZR20010001-冯佳 <fengjia@zhongruigroup.com>
2020-06-19 09:32:28 +0800

    测试本地创建分支，远程是否会有

--- test-extends/src/main/java/com/fj/test/interfacetest/Imp/TestDev.java
+++ test-extends/src/main/java/com/fj/test/interfacetest/Imp/TestDev.java
@@ -3,5 +3,6 @@ package com.fj.test.interfacetest.Imp;
 public class TestDev {
     public static void main(String[] args) {
         System.out.println("I am dev");
+        System.out.println();
     }
 }
